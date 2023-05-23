22b8cb3378479e8d108ef6010637f665e3eb0dcb
mdforhan92 <mdforhan92@gmail.com>
2023-05-23 21:50:07 +0600

    added menu

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,16 +1,11 @@
-import { useEffect, useState } from 'react';
 import SectionTitle from '../../../components/SectionTitle/SectionTitle';
 import MenuItem from '../../../Shared/MenuItem/MenuItem';
+import useMenu from '../../../Hooks/useMwnu';
 
 const PopularMenu = () => {
-    const [menu, setMenu] = useState([]);
-    useEffect(() => {
-        fetch('menu.json')
-        .then(res => res.json())
-        .then(data => {
-            const popularItems = data.filter(item => item.category === 'popular');
-            setMenu(popularItems)})
-    }, [])
+    const [menu] = useMenu();
+    const popular = menu.filter(item => item.category === 'popular');
+   
 
     return (
         <section className='mb-12'>
@@ -20,7 +15,7 @@ const PopularMenu = () => {
             ></SectionTitle>
             <div className='grid md:grid-cols-2 gap-10'>
                 {
-                    menu.map(item => <MenuItem
+                    popular.map(item => <MenuItem
                     key={item.id}
                     item={item}
                     ></MenuItem>)
