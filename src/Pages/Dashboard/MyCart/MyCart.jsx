8e6e1d36abafe239ebe873ff58d1368ccc910ae7
mdforhan92 <mdforhan92@gmail.com>
2023-05-29 10:41:54 +0600

    added totat

--- src/Pages/Dashboard/MyCart/MyCart.jsx
+++ src/Pages/Dashboard/MyCart/MyCart.jsx
@@ -1,8 +1,20 @@
+import { Helmet } from "react-helmet-async";
+import useCart from "../../../Hooks/useCart";
 
 const MyCart = () => {
+    const [cart] = useCart();
+    console.log(cart);
+    const total = cart.reduce((sum, item) => item.price + sum, 0);
     return (
         <div>
-            <h2>my cart</h2>
+            <Helmet>
+                <title>Bistro Boss | My Cart</title>
+            </Helmet>
+            <div>
+                <h3 className="text-3xl">Total Items: {cart.length}</h3>
+                <h3 className="text-3xl">Total Price: {total}</h3>
+                <button className="btn btn-warning btn-sm">PAY</button>
+            </div>
         </div>
     );
 };
