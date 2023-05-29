56bfa34cce2f54e7f6cc13ae09bb8dc2ce51100c
mdforhan92 <mdforhan92@gmail.com>
2023-05-30 00:24:33 +0600

    added jwt

--- src/Pages/AllUsers/AllUsers.jsx
+++ src/Pages/AllUsers/AllUsers.jsx
@@ -1,12 +1,33 @@
 import { useQuery } from "@tanstack/react-query";
 import { Helmet } from "react-helmet-async";
 import { FaRegTrashAlt, FaUserShield } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const AllUsers = () => {
     const {data: users = [], refetch} = useQuery(['users'], async()=>{
         const res = await fetch('http://localhost:3000/users');
         return res.json();
     })
+
+    const handleMakeAdmin = user =>{
+        fetch(`http://localhost:3000/users/admin/${user._id}`, {
+            method: 'PATCH'
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            if(data.modifiedCount){
+                refetch();
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: `${user.name} is an Admin Now!`,
+                    showConfirmButton: false,
+                    timer: 1500
+                  })
+            }
+        })
+    }
 
     const handleDelete = user => {
 
@@ -36,7 +57,7 @@ const AllUsers = () => {
             <th>{index + 1}</th>
             <td>{user.name}</td>
             <td>{user.email}</td>
-            <td>{user.role === 'admin' ? 'admin' : <button onClick={() => handleDelete(user)} className="btn btn-xl text-2xl bg-orange-600 text-white"><FaUserShield></FaUserShield></button>}</td>
+            <td>{user.role === 'admin' ? 'admin' : <button onClick={() => handleMakeAdmin(user)} className="btn btn-xl text-2xl bg-orange-600 text-white"><FaUserShield></FaUserShield></button>}</td>
             <td> <button onClick={() => handleDelete(user)} className="btn btn-xl text-2xl bg-red-600 text-white"><FaRegTrashAlt></FaRegTrashAlt></button></td>
           </tr>)
       }
--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth"
 import app from "../firebase/firebase.config";
+import axios from "axios";
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
@@ -47,6 +48,19 @@ const AuthProvider = ({children}) => {
        const unsubscribe = onAuthStateChanged(auth, currentUser =>{
             setUser(currentUser);
             console.log('currentUser', currentUser);
+
+            // get and set token
+            if(currentUser){
+                axios.post('http://localhost:3000/jwt', {email: currentUser?.email})
+                .then(data => {
+                    // console.log(data.data.token);
+                    localStorage.setItem('access-token', data?.data?.token);
+                })
+            }
+            else{
+                localStorage.removeItem('access-token');
+            }
+
             setLoading(false);
         });
         return () => {
