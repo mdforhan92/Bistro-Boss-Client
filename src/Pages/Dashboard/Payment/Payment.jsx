4e83a1d246fdf6edf13e398fc0d3db7dc6932110
mdforhan92 <mdforhan92@gmail.com>
2023-06-03 22:12:12 +0600

    added payment

---
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -0,0 +1,25 @@
+import { loadStripe } from "@stripe/stripe-js";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import CheckoutForm from "./CheckoutForm";
+import { Elements } from "@stripe/react-stripe-js";
+import useCart from "../../../Hooks/useCart";
+
+//TODO: Add your stripe public key
+const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
+const Payment = () => {
+    const [cart] = useCart();
+    // reduce is a function that takes two arguments, a callback function and an initial value
+    const total = cart.reduce((sum, item) => item.price + sum, 0);
+    const price = parseFloat(total.toFixed(2));
+    return (
+        <div>
+            <SectionTitle heading={'Payment'} subHeading={'Please process'}></SectionTitle>
+            <h2 className="text-3xl">Take  a teka </h2>
+            <Elements stripe={stripePromise}>
+                <CheckoutForm price={price}></CheckoutForm>
+            </Elements>
+        </div>
+    );
+};
+
+export default Payment;
